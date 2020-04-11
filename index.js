acf283dd73394aedf237b440b0cd0166f9b551ce
Jobayer Arman <jobayer.arman@gmail.com>
2020-04-11 22:14:35 +0600

    add function to scrap data

--- index.js
+++ index.js
@@ -4,6 +4,26 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const PORT = process.env.PORT || 5000
 
+const fetchHtml = async (url) => {
+  try {
+    const { data } = await axios.get(url);
+    return data;
+  } catch {
+    console.error(`ERROR: An error occurred while trying to fetch the URL: ${url}`);
+  }
+};
+
+const scrapData = async () => {
+  const webUrl = "https://corona.gov.bd/";
+  const html = await fetchHtml(webUrl);
+  const $ = cheerio.load(html);
+  const statistic = $(".statistic__efected").html();
+
+  console.log(statistic);
+}
+
+scrapData();
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
