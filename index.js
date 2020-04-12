a3a15044d6d76b50ceddb074ca57ebd7753997c7
Jobayer Arman <jobayer.arman@gmail.com>
2020-04-12 18:07:59 +0600

    refactor(index.js): code cleanup

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-const express = require('express');
-const path = require('path');
-const { NovelCovid } = require("novelcovid");
-const track = new NovelCovid();
-const PORT = process.env.PORT || 5000;
+const express        = require('express');
+const app            = express();
+const path           = require('path');
+
+const { NovelCovid } = require('novelcovid');
+const track          = new NovelCovid();
+
+const PORT           = process.env.PORT || 5000;
 
 let cases, todayCases, deaths, todayDeaths;
 
 const assignData = (result) => {
   cases = result.cases;
   todayCases = result.todayCases;
-}
+};
 track.countries("Bangladesh").then(assignData);
 
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
+app
+  .set("view engine", "ejs")
+  .set("views", path.join(__dirname, "views"))
+  .use(express.static(path.join(__dirname, "public")))
 
   // ---- ROUTES ---- //
-  .get('/', (req, res) => res.render('pages/index'))
+  .get("/", (req, res) => {
+    res.render('pages/index');
+  })
   .get('/wiki', (req, res) => res.render('pages/wiki'))
   .get('/about', (req, res) => res.render('pages/about'))
 
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
