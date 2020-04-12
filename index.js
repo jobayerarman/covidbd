00fff1eda69cb36413324da75e01b403bd4f2580
Jobayer Arman <jobayer.arman@gmail.com>
2020-04-12 19:19:28 +0600

    update index.js
    added data variables for ejs template
    removed unneccessary code

--- index.js
+++ index.js
@@ -7,22 +7,30 @@ const track          = new NovelCovid();
 
 const PORT           = process.env.PORT || 5000;
 
-let cases, todayCases, deaths, todayDeaths;
-
-const assignData = (result) => {
-  cases = result.cases;
-  todayCases = result.todayCases;
-};
-track.countries("Bangladesh").then(assignData);
-
 app
-  .set("view engine", "ejs")
-  .set("views", path.join(__dirname, "views"))
-  .use(express.static(path.join(__dirname, "public")))
+  .set('view engine', 'ejs')
+  .set('views', path.join(__dirname, 'views'))
+  .use(express.static(path.join(__dirname, 'public')))
 
   // ---- ROUTES ---- //
-  .get("/", (req, res) => {
-    res.render('pages/index');
+  .get('/', (req, res) => {
+    track.countries('Bangladesh').then((result) => {
+      let todayCases = result.todayCases;
+      let cases = result.cases;
+      let todayDeaths = result.todayDeaths;
+      let deaths = result.deaths;
+      let recovered = result.recovered;
+      let tests = result.tests;
+
+      res.render("pages/index", {
+        todayCases: todayCases,
+        cases: cases,
+        todayDeaths: todayDeaths,
+        deaths: deaths,
+        recovered: recovered,
+        tests: tests,
+      });
+    });
   })
   .get('/wiki', (req, res) => res.render('pages/wiki'))
   .get('/about', (req, res) => res.render('pages/about'))
