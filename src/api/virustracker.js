e84f890b68a33019e4eb2a9d77476001ecb04089
Jobayer Arman <jobayer.arman@gmail.com>
2020-04-29 10:37:08 +0600

    added total and timeline functions

--- src/api/virustracker.js
+++ src/api/virustracker.js
@@ -11,14 +11,55 @@ const fetchJson = (dataToFetch) => fetch(`${baseURL}${dataToFetch}`).json();
 
 /**
  * @description Fetches Country Total
- * @returns {Promise}
+ * @returns {Promise<array>}
  */
-const countryTotal = () => {};
+const countryTotal = async () => {
+  try {
+    let dataURL = '?countryTotal=BD';
+    let {
+      countrydata: [
+        {
+          total_cases,
+          total_recovered,
+          total_unresolved,
+          total_deaths,
+          total_new_cases_today,
+          total_new_deaths_today,
+          total_active_cases,
+          total_serious_cases,
+          total_danger_rank,
+        },
+      ],
+    } = await fetchJson(dataURL);
+
+    return {
+      cases: total_cases,
+      recovered: total_recovered,
+      unresolved: total_unresolved,
+      deaths: total_deaths,
+      new_cases_today: total_new_cases_today,
+      new_deaths_today: total_new_deaths_today,
+      active_cases: total_active_cases,
+      serious_cases: total_serious_cases,
+      danger_rank: total_danger_rank,
+    };
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 /**
  * @description Fetches Country Timeline
- * @returns {Promise}
+ * @returns {Promise<array>}
  */
-const countryTimeline = () => {};
+const countryTimeline = async () => {
+  try {
+    let dataURL = '?countryTimeline=BD';
+    let { timelineitems } = await fetchJson(dataURL);
+    return timelineitems;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 module.exports = { countryTotal, countryTimeline };
